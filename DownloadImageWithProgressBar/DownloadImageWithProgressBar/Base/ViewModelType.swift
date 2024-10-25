37c2851a400705a7128f4cf580033b0ee2f0d444
huda elhady <huda.m.elhady@gmail.com>
2024-10-26 00:05:49 +0300

    add extension for String and View

--- DownloadImageWithProgressBar/DownloadImageWithProgressBar/Base/ViewModelType.swift
+++ DownloadImageWithProgressBar/DownloadImageWithProgressBar/Base/ViewModelType.swift
@@ -1,5 +1,14 @@
 
 import Foundation
+import Combine
+
+public typealias AnyUIEvent<T> = AnySubject<T, Never>
+
+public extension AnyUIEvent {
+    static func create<T>() -> AnyUIEvent<T> {
+        return PassthroughSubject<T, Never>().eraseToAnySubject()
+    }
+}
 
 protocol ViewModelType {
     associatedtype Input
---
+++ DownloadImageWithProgressBar/DownloadImageWithProgressBar/Extensions/String.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+extension String: LocalizedError {
+  public var errorDescription: String? {
+    return self
+  }
+}
