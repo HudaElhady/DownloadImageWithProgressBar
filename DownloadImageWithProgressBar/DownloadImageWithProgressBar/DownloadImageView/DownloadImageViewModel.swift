8f9aa0b4842038fe813637a0f29338b23ffa256c
huda elhady <huda.m.elhady@gmail.com>
2024-10-26 00:07:57 +0300

    update DownloadImageViewModel to observe the downloadProgress from the repository and update the imageData

--- DownloadImageWithProgressBar/DownloadImageWithProgressBar/DownloadImageView/DownloadImageViewModel.swift
+++ DownloadImageWithProgressBar/DownloadImageWithProgressBar/DownloadImageView/DownloadImageViewModel.swift
@@ -8,22 +8,60 @@ protocol DownloadImageViewModelProtocol {
 
 extension DownloadImageViewModel {
     class Input: ObservableObject {
-        
+        let downloadImageTrigger: AnyUIEvent<Void> = .create()
     }
 }
 
 extension DownloadImageViewModel {
     class Output: ObservableObject {
-        
+        @Published var imageData: Data?
+        @Published var progress: Double = 0
     }
 }
 
 class DownloadImageViewModel: ViewModel, ViewModelType {
     var input: Input
     var output: Output
-    
-    override init() {
+    let repository: ImageRepositoryProtocol
+
+    init(repository: ImageRepositoryProtocol = ImageRepository()) {
         self.input = .init()
         self.output = .init()
+        self.repository = repository
+        
+        super.init()
+        
+        setupObservables()
+    }
+}
+
+private extension DownloadImageViewModel {
+    func setupObservables() {
+        observeDownloadImageTrigger()
+        observeDownloadProgress()
+    }
+
+    func observeDownloadImageTrigger() {
+        input
+            .downloadImageTrigger
+            .sink { [weak self] in
+                self?.getImage()
+            }
+            .store(in: &cancellables)
+    }
+    
+    func observeDownloadProgress() {
+        repository
+            .downloadProgress
+            .assign(to: &output.$progress)
+    }
+
+    func getImage() {
+        Task {
+            @MainActor in
+            let result = try? await repository.fetchImage()
+            guard let image = result else { return }
+            output.imageData = image
+        }
     }
 }
